b4e2c9ac756564c8d58d80805c9471e7f8275256
Julia3202 <redkina_julia7@mail.ru>
2023-12-13 22:38:44 +0300

    create new classes

--- main-service/src/main/java/ru/practicum/exception/ErrorHandler.java
+++ main-service/src/main/java/ru/practicum/exception/ErrorHandler.java
@@ -1,7 +1,9 @@
 package ru.practicum.exception;
 
-import lombok.extern.slf4j.Slf4j;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.BindException;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -10,14 +12,12 @@ import java.time.LocalDateTime;
 
 import static ru.practicum.validator.Constants.DATE_TIME_FORMATTER;
 
-@Slf4j
 @RestControllerAdvice
 public class ErrorHandler {
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public ApiError handleException(final OtherException e) {
-        log.error("Error 500: {}", "Произошла внутренняя ошибка сервера.");
         String reason = "Произошла внутренняя ошибка сервера.";
         return ApiError.builder()
                 .message(e.getMessage())
@@ -52,6 +52,30 @@ public class ErrorHandler {
     }
 
     @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ApiError handleBindException(final BindException e) {
+        return ApiError.builder()
+                .status(HttpStatus.BAD_REQUEST.name())
+                .reason("Incorrectly made request.")
+                .message("Field: " + e.getFieldError().getField() +
+                        ". Error: " + e.getFieldError().getDefaultMessage() +
+                        ". Value: " + e.getFieldError().getRejectedValue())
+                .timestamp(LocalDateTime.now().format(DATE_TIME_FORMATTER))
+                .build();
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ApiError handleBindException(final MissingServletRequestParameterException e) {
+        return ApiError.builder()
+                .status(HttpStatus.BAD_REQUEST.name())
+                .reason("Incorrectly made request.")
+                .message(e.getMessage())
+                .timestamp(LocalDateTime.now().format(DATE_TIME_FORMATTER))
+                .build();
+    }
+
+    @ExceptionHandler
     @ResponseStatus(HttpStatus.CONFLICT)
     public ApiError handleConflictException(final ConflictException e) {
         String reason = "Запрос кофликтует с текущим состоянием сервера.";
@@ -59,6 +83,17 @@ public class ErrorHandler {
                 .message(e.getMessage())
                 .reason(reason)
                 .status(HttpStatus.CONFLICT.name())
+                .timestamp(LocalDateTime.now().format(DATE_TIME_FORMATTER))
+                .build();
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.CONFLICT)
+    public ApiError handleDataIntegrityViolationException(final DataIntegrityViolationException e) {
+        return ApiError.builder()
+                .status(HttpStatus.CONFLICT.name())
+                .reason("Integrity constraint has been violated.")
+                .message(e.getMessage())
                 .timestamp(LocalDateTime.now().format(DATE_TIME_FORMATTER))
                 .build();
     }
