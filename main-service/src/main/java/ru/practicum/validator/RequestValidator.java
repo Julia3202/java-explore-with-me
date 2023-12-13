c026dc68a9ee345714be48a74852f8fb377a9572
Julia3202 <redkina_julia7@mail.ru>
2023-12-13 21:33:10 +0300

    create new classes

--- main-service/src/main/java/ru/practicum/validator/RequestValidator.java
+++ main-service/src/main/java/ru/practicum/validator/RequestValidator.java
@@ -1,17 +1,10 @@
 package ru.practicum.validator;
 
-import ru.practicum.event.model.Event;
 import ru.practicum.exception.ConflictException;
 import ru.practicum.request.model.Request;
 import ru.practicum.user.model.User;
 
 public class RequestValidator {
-    public void validRequester(Long userId, Event event) {
-        if (event.getInitiator().getId() == userId) {
-            throw new ConflictException("Нельзя делать запрос на мероприятие, которое создали Вы сами.");
-        }
-    }
-
     public void existRequester(Request request, User user) {
         if (request.getRequester().equals(user)) {
             throw new ConflictException("Запрос с ID-" + request.getId() + " не был создан пользователем с ID-" +
